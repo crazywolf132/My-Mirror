6a550fc0200fd3f7cf93e64bc9b632e7b4f568f6
Crazywolf132 <crazywolf132@gmail.com>
2017-10-16 22:18:52 +1100

    Gave asher his own part in view.js

--- js/view.js
+++ js/view.js
@@ -8,4 +8,14 @@ var View = function() {
       notesEl.text("")
     }, time)
   }
+
+  this.asherMessage = function(Msg) {
+    notesEl.text(Msg)
+    notesEl.css('color', '#e67e22')
+    var time = 5500 + (Msg.length * 20);
+    setTimeout(function(){
+      notesEl.text("")
+      notesEl.css('color', '#fff')
+    }, time)
+  }
 }
